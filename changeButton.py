ddc7fbd591f6207c2c44caac9d3ae1c0e70448af
Choi <csw3055@gmail.com>
2020-09-14 09:29:28 +0900

    init

---
+++ changeButton.py
@@ -0,0 +1,17 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+class button(QtWidgets.QLabel, QtWidgets.QFrame):
+    def __init__(self, parent=None):
+        QtWidgets.QLabel.__init__(self, parent)
+        QtWidgets.QFrame.__init__(self, parent)
+        self.signal = False
+    clicked = QtCore.pyqtSignal()
+
+    
+    def mouseReleaseEvent(self, null):
+        if self.signal == True:
+            self.clicked.emit()
+            self.signal = False
+            
+    def mousePressEvent(self, null):
+        self.signal = True
+
